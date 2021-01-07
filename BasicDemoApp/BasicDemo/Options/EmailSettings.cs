f17a5faa53d39ff6668024ab15047ef4210fd831
Md Risul Karim <risulkarimx1@gmail.com>
2021-01-07 23:22:48 +0100

    added EmailSettings.cs

---
+++ BasicDemoApp/BasicDemo/Options/EmailSettings.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BasicDemo.Options
+{
+    public class EmailSettings
+    {
+        public bool EnableSystems { get; set; }
+
+        public double EmailTimeOutInSeconds { get; set; }
+
+        public List<string> EmailServers { get; set; }
+
+        public override string ToString()
+        {
+            var servers = new StringBuilder();
+            EmailServers.ForEach(es=>servers.Append($"{es},"));
+            return $"Enable: {EnableSystems}, TimeOut: {EmailTimeOutInSeconds}, " +
+                   $"Servers : {servers}";
+        }
+    }
+}
